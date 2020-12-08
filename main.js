a5d7595d63043c9ffaaea1f7f40ea3e294153cca
Purvesh Phirke <69496853+Purvesh06@users.noreply.github.com>
2020-12-08 16:55:06 +0530

    Puru interior Design

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function openBar() {
+    let overlay = document.getElementById('overlay');
+    overlay.style.display = 'block'; 
+}
+
+function closebar() {
+    let overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+}
